d67bcabadf2b0cb7b7e2be891f9d69d1fc9be5e2
Artur Sachakov <artur_sachakov@yahoo.com>
2018-04-09 23:29:52 -0400

    The-Lighthouse-Website

---
+++ list.js
@@ -0,0 +1,90 @@
+/*
+   New Perspectives on JavaScript
+   Tutorial 3
+   Case Problem 1
+
+   Filename: list.js
+
+   Variable List:
+
+   firstName: Contains the first name of the contributors.
+   lastName:  Contains the last name of the contributors.
+   street:    Contains the contributor's street address
+   city:      Contains the contributor's city of residence
+   state:     Contains the contributor's state of residence
+   zip:       Contains the contributor's zip code.
+   amount:    Contains the amount of the contribution.
+   date:      Contains the date of the contribution.
+
+*/
+
+firstName = new Array();
+lastName = new Array();
+street = new Array();
+city = new Array();
+state= new Array();
+zip = new Array();
+amount = new Array();
+date = new Array();
+
+
+firstName[0]="Nina";
+lastName[0]="Largent";
+street[0]="88 Regal Lane";
+city[0]="Wheaton";
+state[0]="KY";
+zip[0]="89780";
+amount[0]=125;
+date[0]="2007-09-18";
+
+
+firstName[1]="Mike";
+lastName[1]="Hunt";
+street[1]="404 Barrow Street";
+city[1]="Drake";
+state[1]="KY";
+zip[1]="88841";
+amount[1]=75;
+date[1]="2007-09-18";
+
+
+firstName[2]="Monica";
+lastName[2]="Lang";
+street[2]="743 Stawlings Drive";
+city[2]="Youngston";
+state[2]="KY";
+zip[2]="89747";
+amount[2]=50;
+date[2]="2007-09-16";
+
+
+firstName[3]="William";
+lastName[3]="Mcknight";
+street[3]="102 Maple Lane";
+city[3]="Youngston";
+state[3]="KY";
+zip[3]="88660";
+amount[3]=150;
+date[3]="2007-09-15";
+
+
+firstName[4]="Latrina";
+lastName[4]="Hults";
+street[4]="750 Whitehall Road";
+city[4]="Drake";
+state[4]="KY";
+zip[4]="89664";
+amount[4]=250;
+date[4]="2007-09-14";
+
+
+firstName[5]="Danny";
+lastName[5]="Shamblin";
+street[5]="123 Smith Drive";
+city[5]="Delphi";
+state[5]="KY";
+zip[5]="88585";
+amount[5]=50;
+date[5]="2007-09-13";
+
+
